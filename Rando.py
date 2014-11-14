73bcd544dba9f7e1c48c04067d3a907c479254d9
ryankapler <rkapler@ncsu.edu>
2014-11-14 00:55:58 -0500

    Update Rando.py

--- Rando.py
+++ Rando.py
@@ -1,1 +1,13 @@
 #Random number generator
+#to be done
+amount = raw_input("How many random numbers do you need: ")
+Num_max = raw_input("What is the max value you need: ")
+Num_min = raw_input("What is the min value you need: ")
+num_list = []
+from random import randint
+for i in range(amount):
+    num_list[i] = randint(Num_min, Num_max)
+
+print numlist
+
+    
